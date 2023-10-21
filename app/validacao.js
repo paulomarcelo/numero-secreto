503b90d4da91e1785e13880a31c3ba5b28d35912
paulomarcelo <paulo.marcelo@gmail.com>
2023-10-21 11:34:09 -0300

    formatacao do codigo

--- app/validacao.js
+++ app/validacao.js
@@ -7,8 +7,9 @@ function verificaSeOChutePossuiUmValorValido(chute) {
     }
 
     if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
-        elementoChute.innerHTML += 
-        `<div>Valor inválido: Fale um número entre ${menorValor} e ${maiorValor}</div>`
+        elementoChute.innerHTML += `
+        <div>Valor inválido: Fale um número entre ${menorValor} e ${maiorValor}</div>
+        `
         return
     }
 
@@ -17,21 +18,22 @@ function verificaSeOChutePossuiUmValorValido(chute) {
             <h2>Você acertou!</h2>
             <h3>O número secreto era ${numeroSecreto}</h3>
         `
-    } else if(numero > numeroSecreto) {
+    } else if (numero > numeroSecreto) {
         elementoChute.innerHTML += `
-            <div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>`
+        <div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>
+        `
     } else {
         elementoChute.innerHTML += `
-            <div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>`
+        <div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>
+        `
     }
-
+ 
 }
 
 function chuteForInvalido(numero) {
     return Number.isNaN(numero)
 }
 
-
-function numeroForMaiorOuMenorQueOValorPermitido(numero) {
+function numeroForMaiorOuMenorQueOValorPermitido(numero){
     return numero > maiorValor || numero < menorValor
 }
